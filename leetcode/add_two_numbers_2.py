bdb2f5f4ffc3acd3726d0a84d6528d40da7957a4
mokhs00 <mokhs00@naver.com>
2021-10-17 21:31:43 +0900

    update : use Full Adder in leetcode add_two_numbers_2.py

--- leetcode/add_two_numbers_2.py
+++ leetcode/add_two_numbers_2.py
@@ -42,3 +42,25 @@ class Solution:
         resultStr = int(''.join(map(str, a))) + int(''.join(map(str, b)))
 
         return self.toReversedLinkedList(str(resultStr))
+
+
+
+    def addTwoNumbers_use_full_adder(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        root = head = ListNode(0)
+
+        carry = 0
+        while l1 or l2 or carry:
+            sum = 0
+
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+
+            carry, val = divmod(sum + carry, 10)
+            head.next = ListNode(val)
+            head = head.next
+
+        return root.next
