90dd179a5466a22eeca3b240a643f840a7fb3f01
mokhs00 <mokhs00@naver.com>
2021-10-24 18:15:20 +0900

    fix typo

--- leetcode/daily_temperatures_739.py
+++ leetcode/daily_temperatures_739.py
@@ -1,9 +1,9 @@
 from typing import List
 '''
-stack 에는 index 를 담고 -> index 로 temperaetures 에서 값을 조회하기 위함
+stack 에는 index 를 담고 -> index 로 temperatures 에서 값을 조회하기 위함
 answer[]에 기본 값으로 0을 채워둔다.
 temperatures 를 탐색하며 stack[-1] 값과 비교하여 stack[-1]이 현재 temperature 보다 값이 크다면, 
-기존에 stack 에 있던 값을 pop 하고 last라는 변수에 담아두고,
+기존에 stack 에 있던 값을 pop 하고 last 라는 변수에 담아두고,
  (현재 temperature 의 index) - (stack[-1]) 값을 answer[last] 에 기록한다. 
 '''
 
