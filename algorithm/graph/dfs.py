1ae350a5987a76ae5089a1723e002e75fea980d5
mokhs00 <mokhs00@naver.com>
2021-11-14 18:59:13 +0900

    add : iterative_dfs() to dfs

--- algorithm/graph/dfs.py
+++ algorithm/graph/dfs.py
@@ -17,4 +17,20 @@ def recursive_dfs(v, discovered=[]):
             discovered = recursive_dfs(w, discovered)
     return discovered
 
+
 # print(recursive_dfs(1))
+
+
+def iterative_dfs(start_v):
+    discovered = []
+    stack = [start_v]
+    while stack:
+        v = stack.pop()
+        if v not in discovered:
+            discovered.append(v)
+            for w in graph[v]:
+                stack.append(w)
+
+    return discovered
+
+# print(iterative_dfs(1))
