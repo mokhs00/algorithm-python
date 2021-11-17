16706664b64ae1728b20713d950bcb8db0fa389d
mokhs00 <mokhs00@naver.com>
2021-11-17 22:02:00 +0900

    update : add other solution to leetcode permutations_46

--- leetcode/permutations_46.py
+++ leetcode/permutations_46.py
@@ -10,7 +10,7 @@ class Solution:
 
         def dfs(prev_list: List, num_list: List[int]):
 
-            if len(prev_list) == size or num_list is None:
+            if len(prev_list) == size:
                 result.append(prev_list)
                 return
 
@@ -20,5 +20,25 @@ class Solution:
                 dfs(prev_list + [num], temp_list)
 
         dfs([], nums)
+
+        return result
+
+    def permute_2(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        prev_elements = []
+
+        def dfs(num_list: List):
+            if len(num_list) == 0:
+                result.append(prev_elements[:])
+
+            for num in num_list:
+                next_list = num_list[:]
+                next_list.remove(num)
+
+                prev_elements.append(num)
+                dfs(next_list)
+                prev_elements.pop()
+
+        dfs(nums)
 
         return result
