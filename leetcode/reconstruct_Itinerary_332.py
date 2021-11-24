6c6bc0ac5d9d2fca130e2defc544cad0c3688c74
mokhs00 <mokhs00@naver.com>
2021-11-24 13:55:44 +0900

    update : 역순 정렬을 이용해 성능 개선 in leetcode reconstruct_Itinerary_332.py

--- leetcode/reconstruct_Itinerary_332.py
+++ leetcode/reconstruct_Itinerary_332.py
@@ -6,14 +6,14 @@ class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         graph = collections.defaultdict(list)
 
-        for i, j in sorted(tickets):
+        for i, j in sorted(tickets, reverse=True):
             graph[i].append(j)
 
         route = []
 
         def dfs(start):
             while graph[start]:
-                dfs(graph[start].pop(0))
+                dfs(graph[start].pop())
             route.append(start)
 
         dfs('JFK')
