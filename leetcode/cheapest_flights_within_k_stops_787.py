3c1a389d3a4c2e42fc85955cd62030960447137f
mokhs00 <mokhs00@naver.com>
2021-12-27 23:56:57 +0900

    update : cheapest_flights_within_k_stops_787.py
    이미 방문한 경우를 검사하여 시간 단축

--- leetcode/cheapest_flights_within_k_stops_787.py
+++ leetcode/cheapest_flights_within_k_stops_787.py
@@ -6,20 +6,19 @@ from typing import List
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         graph = collections.defaultdict(list)
-
         for u, v, w in flights:
-            graph[u].append((v, w))
+            graph[u].append([v, w])
 
-        Q = [(0, src, k)]
-
+        visit = {}
+        Q = [(0, src, 0)]
         while Q:
-            price, node, k = heapq.heappop(Q)
-
+            price, node, k_ = heapq.heappop(Q)
             if node == dst:
                 return price
-            if k >= 0:
+            if node not in visit or visit[node] > k_:
+                visit[node] = k_
                 for v, w in graph[node]:
-                    alt = price + w
-                    heapq.heappush(Q, (alt, v, k - 1))
-
+                    if k_ <= k:
+                        alt = price + w
+                        heapq.heappush(Q, (alt, v, k_ + 1))
         return -1
